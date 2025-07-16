776691799ef6ae03cc099b08085defc7b7056900
Martin Ojom <martinojom28@gmail.com>
2025-07-16 14:52:08 +0200

    Grid cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,12 @@ function App() {
   return (
     <Grid
       templateAreas={{
-        base: `"nav main"`,
+        base: `"nav" " main"`,
         lg: `"nav nav" "aside main"`,
       }}
       templateColumns={{
         base: "1fr",
-        lg: "200px 1fr",
+        lg: "270px 1fr",
       }}
     >
       <GridItem area="nav">
