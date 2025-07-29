1538c88bd5b20daa2590e6742586f7038d367632
Martin Ojom <martinojom28@gmail.com>
2025-07-29 13:03:00 +0200

    add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, HStack, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "@/services/Image-url";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -19,7 +20,10 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Card.Title textStyle="2xl">{game.name}</Card.Title>
+        <Card.Title textStyle="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Card.Title>
       </Card.Body>
     </Card.Root>
   );
